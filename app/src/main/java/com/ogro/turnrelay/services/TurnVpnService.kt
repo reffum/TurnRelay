35c91d24d16bfb0064e34f777b38498612b46b6e
Oleg Gavrilchenko <oleggavrilcenko6@gmail.com>
2026-04-12 20:28:34 +0300

    Write template VpnService code

--- app/src/main/java/com/ogro/turnrelay/services/TurnVpnService.kt
+++ app/src/main/java/com/ogro/turnrelay/services/TurnVpnService.kt
@@ -1,13 +1,22 @@
 package com.ogro.turnrelay.services
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.net.VpnService
 import android.os.ParcelFileDescriptor
+import android.util.Log
 
+@SuppressLint("VpnServicePolicy")
 class TurnVpnService : VpnService() {
+    companion object {
+        const val TAG = "TurnVpnService"
+    }
+
     private var vpnInterface: ParcelFileDescriptor? = null
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        Log.i(TAG, "TurnVpnService started")
+
         // Configure the VPN
         val builder = Builder()
             .setSession("TurnVpnService")
@@ -21,11 +30,13 @@ class TurnVpnService : VpnService() {
     }
 
     override fun onRevoke() {
+        Log.i(TAG, "Service revoked")
         closeInterface()
         super.onRevoke()
     }
 
     override fun onDestroy() {
+        Log.i(TAG, "Service destroyed")
         closeInterface()
         super.onDestroy()
     }
